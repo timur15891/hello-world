7658ff47cc53819b50275d3fb66db5739ac0ec06
Mouzitoto <Mouzitoto@gmail.com>
2016-03-12 23:29:00 +0600

    renamed class, added directories

--- src/java/Test.java
+++ src/java/Test.java
@@ -1,4 +1,8 @@
-public class test
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test
 {
     public static void main(String args[]) throws IOException
     {
